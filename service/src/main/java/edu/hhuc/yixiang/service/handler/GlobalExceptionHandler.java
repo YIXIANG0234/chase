2bbaff74d88b1c70adf7bba2eb424d73e055acfb
guwanghuai <guwanghuai@pdd.com>
2023-12-28 10:31:50 +0800

    增加logback日志配置

--- service/src/main/java/edu/hhuc/yixiang/service/handler/GlobalExceptionHandler.java
+++ service/src/main/java/edu/hhuc/yixiang/service/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package edu.hhuc.yixiang.service.handler;
 import edu.hhuc.yixiang.common.base.BaseResponse;
 import edu.hhuc.yixiang.common.base.BaseResultCode;
 import edu.hhuc.yixiang.common.exception.ChaseBaseException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,17 +16,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @description
  * @date 2023/12/27 21:13:39
  */
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ResponseStatus(value = HttpStatus.OK)
     @ExceptionHandler(value = ChaseBaseException.class)
     public BaseResponse<Void> handlerChaseBaseException(ChaseBaseException e) {
+        log.error(e.getMessage(), e);
         return BaseResponse.ofFailure(e.getErrorCode(), e.getMessage());
     }
 
     @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(value = RuntimeException.class)
-    public BaseResponse<Void> handlerChaseBaseException(RuntimeException e) {
+    public BaseResponse<Void> handlerRuntimeException(RuntimeException e) {
+        log.error(e.getMessage(), e);
         return BaseResponse.ofFailure(BaseResultCode.INTERNAL_SERVER_ERROR);
     }
 }
---
+++ todo.md
@@ -0,0 +1,25 @@
+### 功能点
+
+- [x] gradle多模块项目搭建
+  - 需要后续完善和优化
+- [x] mybatis接入
+  - mybatis-flex
+- [x] redis接入
+  - RedisHelper
+- [x] redis锁实现
+  - DistributedLock, DistributedLockAspect
+- [ ] 各种工具类实现
+  - DateUtil
+  - JsonUtil
+- [x] 统一返回结果
+  - BaseResponse，BaseResultCode
+- [x] 统一异常处理
+  - GlobalExceptionHandler，ChaseBaseException
+- [x] logback日志配置
+  - https://juejin.cn/post/7200549600590282789
+- [ ] 无侵入式操作日志实现
+- [ ] 线程池对象封装
+- [ ] 通用异步轮询接口
+- [ ] excel导出
+- [ ] excel导入
+- [ ] 通用分页，排序请求对象封装
